cfabeacfa5e141e5d3962e730e236034f81be6d5
AashishKumar(RA2111028010057) <103728139+Aashish-wj@users.noreply.github.com>
2024-04-25 15:20:45 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 // const mongoDbClient = require("mongodb").MongoClient
 const mongoURI =
-"mongodb+srv://vercel-admin-user-6628e6d50382c20a22d1b3a8:4hlMCRQSFzxWSJyj@cluster0.pjuibh4.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0";
+"mongodb+srv://am2567:2F6jsmUXeE1GYjiT@cluster0.pjuibh4.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0";
 module.exports = function (callback) {
   mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
     // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
